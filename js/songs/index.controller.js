b5f6d47cfc10546d602d5e45e9ce34f20ddbf6b3
Andy <andrew.sunglae.kim@gmail.com>
2015-12-10 10:28:30 -0500

    can play songs

--- js/songs/index.controller.js
+++ js/songs/index.controller.js
@@ -5,14 +5,17 @@
     .module("songs")
     .controller("SongsIndexController", [
       "SongFactory",
+      "$sce",
       SongsIndexControllerFunction
     ])
-    function SongsIndexControllerFunction (SongFactory){
+    function SongsIndexControllerFunction (SongFactory, $sce){
       this.songs = SongFactory.query();
       this.newSong = new SongFactory();
       this.play = function(song){
         console.log("i was clicked", song);
         this.playSong = song
+        // workaround for ng-src not allowing expression {{song.audio_url}}
+        this.playSong.encodedUrl = $sce.trustAsResourceUrl(song.audio_url)
         console.log(this);
       }
     }
